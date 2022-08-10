6bf4c0a180597377674bb8df0d7db3ddf472ad44
niatran93 <106176828+niatran93@users.noreply.github.com>
2022-08-10 09:54:45 +0700

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,2 @@
-# Start X at login interactive mode
-if status --is-interactive
-  if test -z "$DISPLAY" -a $XDG_VTNR = 1
-    exec startx -- -keeptty
-  end
-end
-
 # Disable fish greeting message
 set -g -x fish_greeting
